8f0533bb43b7c8a674a3108718a3ced829c6dc25
jfambro <Jonathan.Fambro@careerbuilder.com>
2016-03-08 13:34:34 -0500

    Completed exercise

---
+++ exercise6.rb
@@ -0,0 +1,27 @@
+#create a variable name = types_of_people and initialize to ten people"
+types_of_people = 10
+# set the x variable to a string and includiing the variable types of people
+x = "There are #{types_of_people} types of people"
+# create a string variable name binary, do_not , y
+# and set it equal to binary string and do_not and
+# set the y variable to a string and including the binary and do_not variable
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+#Print both x and y
+puts x
+puts y
+#Print statements
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+#create boolean variable and set it equal to false
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#print out string with the boolean variable included
+puts joke_evaluation
+
+w = "This is the left side of ..."
+e = "a string with a right side."
+#Append both string variables w and e together
+puts w + e
+
