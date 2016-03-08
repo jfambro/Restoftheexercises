6a77b1d82fed929286cc6ea2e997518ade74a4e9
jfambro <Jonathan.Fambro@careerbuilder.com>
2016-03-08 14:50:53 -0500

    Completed a new file and ruby statement

---
+++ README1.md
@@ -0,0 +1,4 @@
+In exercise 7 I am going through the motions.
+Initializing variables and print out the output.
+In addition, I had to create comments to explain what each line does 
+in the code. 
---
+++ exercise7.rb
@@ -0,0 +1,22 @@
+#print the string and a new line
+puts "Mary had little lamb."
+#print the string and the string in between the brackets"
+puts "Its fleece was white as #{'snow'}"
+#print ten dots
+puts "." * 10 # what'd that do?
+#created 12 variables and initalized them with characters
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+#created and appended variables together
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
