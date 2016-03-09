b6ed3412e34c2f0cb199635d1a44cdd539d71aa0
jfambro <Jonathan.Fambro@careerbuilder.com>
2016-03-09 10:25:00 -0500

    Figure out the reason for the code not working

---
+++ exercise13.rb
@@ -0,0 +1,13 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+
+#Exercise
+one, two = ARGV
+
+puts "You are rank number #{one} in your boixng division"
+puts "You are rank number #{two} in your boxing division"
+
